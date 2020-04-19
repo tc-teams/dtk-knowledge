2e1a9091d6554a0bc09a2801d346738de01c067b
idasilva6 <isaias.carvalho986@gmail.com>
2020-04-19 13:52:44 -0300

    add a new file in project - web crawler

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly/v2"
+)
+
+func main() {
+	c := colly.NewCollector(
+
+	)
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://g1.globo.com/")
+}
